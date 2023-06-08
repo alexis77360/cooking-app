82bcc73658485a8779eac99b6d281e49ed67a446
alexis77360 <84974466+alexis77360@users.noreply.github.com>
2023-06-08 20:49:53 +0200

    ajout design + vidéo

--- src/App.js
+++ src/App.js
@@ -2,38 +2,42 @@ import React, { useEffect,useState } from 'react';
 import axios from 'axios';
 import Card from './components/Card';
 
-
-
 const App = () => {
 
-  const [mealsData, setMealsData] = useState([])
+  const [mealsData, setMealsData] = useState([]);
+  const [inputSearch, setInputSearch] = useState('');
 
 
-  //? création d'un hook d'effet pour récupérer les données de l'API au chargement de la page
+  //? création d'un hook d'effet pour récupérer les données de l'API au chargement de la page de que inoputSearch change je relance le hook 
   useEffect(() => {
     axios
-      .get('https://www.themealdb.com/api/json/v1/1/search.php?s=chicken')
+      .get('https://www.themealdb.com/api/json/v1/1/search.php?s=' + inputSearch)
       .then((res) => setMealsData(res.data.meals));
-  }, []);
+  }, [inputSearch]);
   
-
-
-
-
 
   return (
     <div className="app-container">
-      <h1>React Cooking app</h1>
-      <input type="text" placeholder="Taper le nom de l'aliment en anglais" />
+      <h1>Cooking Receipe</h1>
+      <input 
+      type="text" 
+      autoFocus
+      placeholder="What do you want to eat today?"
+      onChange={(e) => setInputSearch(e.target.value)}
+      />
       <div className="meals-container">
-      {mealsData.map((meal) => 
+        {/*//? slice limite le resultat a 24 si mealsdata existe alors je map sur le tableau et je retourne un composant card avec les props meal */}
+        {mealsData  && mealsData 
+          .slice(0, 24)
+          .map((meal) => 
 
         <Card key={meal.idMeal} meal={meal} />
 
         )}
 
-        
-
+        {/*//? si mealsdata n'existe pas alors je retourne un message d'erreur */}
+        {!mealsData && <p> No result found 🫤</p>}
+      
       </div>
 
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,26 @@
 import React from 'react';
 
 const Card = ({meal}) => {
+
     return (
         <div className='meal-card'>
             <h2>{meal.strMeal}</h2>
             <p>Origin : {meal.strArea}</p>
             <img src={meal.strMealThumb} alt={"photo " + meal.strMeal} />
-            <p>{meal.strInstructions}</p>
-            
+                <h3>Ingredients :</h3>
+                <ul>
+                    {/* //? je map sur les 20 ingredients et je retourne un li avec le nom de l'ingredient*/}
+                    {Object.keys(meal)
+                    .filter((key) => key.includes('Ingredient') && meal[key])
+                    .map((ingredient, index) => (
+                        <li key={index}>
+                            {meal[ingredient]}
+                        </li>
+                    ))}
+                </ul>
+
+                <button onClick={() =>  window.open(meal.strYoutube, "_blank")}> Watch the video</button>
+    
         </div>
     );
 };
