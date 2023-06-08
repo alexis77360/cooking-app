2d200b41798f09a24da988121b9ca6c8e3f94211
alexis77360 <84974466+alexis77360@users.noreply.github.com>
2023-06-08 17:14:53 +0200

    appel d'api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect,useState } from 'react';
+import axios from 'axios';
+import Card from './components/Card';
 
-function App() {
+
+
+const App = () => {
+
+  const [mealsData, setMealsData] = useState([])
+
+
+  //? création d'un hook d'effet pour récupérer les données de l'API au chargement de la page
+  useEffect(() => {
+    axios
+      .get('https://www.themealdb.com/api/json/v1/1/search.php?s=chicken')
+      .then((res) => setMealsData(res.data.meals));
+  }, []);
+  
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1>React Cooking app</h1>
+      <input type="text" placeholder="Taper le nom de l'aliment en anglais" />
+      <div className="meals-container">
+      {mealsData.map((meal) => 
+
+        <Card key={meal.idMeal} meal={meal} />
+
+        )}
+
+        
+
+      </div>
+
+
     </div>
   );
 }
